545c0a372d8674c60b97af12486b32f051ec8231
Willian Tomaz <willianlima28@hotmail.com>
2022-10-13 08:49:55 -0300

    feat: Adicionando exemplo: aplicando arquivo JSON no K8s

--- apply_files_k8s/main.py
+++ apply_files_k8s/main.py
@@ -3,5 +3,11 @@ from kubernetes import client, config, utils
 config.load_kube_config()
 
 k8s_client = client.ApiClient()
-yaml_file = 'apply_files_k8s/meu-pod.yml'
+
+# APLICANDO COM ARQUIVO YML
+yaml_file = 'apply_files_k8s/meu-pod.meu-pod.yml'
 utils.create_from_yaml(k8s_client, yaml_file)
+
+# APLICANDO COM ARQUIVO JSON
+json_file = 'apply_files_k8s/meu-pod.json'
+utils.create_from_yaml(k8s_client, json_file)
