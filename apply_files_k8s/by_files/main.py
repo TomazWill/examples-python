65c20a1e02c8591e8b27fd8b0672d5aedcef71cc
Willian Tomaz <willianlima28@hotmail.com>
2022-10-13 13:38:56 -0300

    feat: Adicionando exemplos de aplicar arquivos no k8s

--- apply_files_k8s/by_files/main.py
+++ apply_files_k8s/by_files/main.py
@@ -1,13 +1,14 @@
 from kubernetes import client, config, utils
 
-config.load_kube_config()
 
+config.load_kube_config()
 k8s_client = client.ApiClient()
 
+
 # APLICANDO COM ARQUIVO YML
-yaml_file = 'apply_files_k8s/meu-pod.meu-pod.yml'
+yaml_file = 'apply_files_k8s/by_files/meu-pod.yml'
 utils.create_from_yaml(k8s_client, yaml_file)
 
 # APLICANDO COM ARQUIVO JSON
-json_file = 'apply_files_k8s/meu-pod.json'
+json_file = 'apply_files_k8s/by_files/meu-pod.json'
 utils.create_from_yaml(k8s_client, json_file)
---
+++ apply_files_k8s/by_object_json/main.py
@@ -0,0 +1,60 @@
+import yaml, json, sys, tempfile, os
+from kubernetes import client, config, utils
+
+
+# My Object JSON
+my_object_json = json.dumps(
+    {
+        "metadata": {
+            "name": "my-test-pod"
+        },
+        "spec": {
+            "containers": [
+                {
+                    "name": "test-container",
+                    "image": "nginx",
+                    "env": [
+                        {
+                            "name": "ENV",
+                            "value": "dev"
+                        }
+                    ]
+                }
+            ]
+        },
+        "apiVersion": "v1",
+        "kind": "Pod"
+    }
+)
+
+# ------------------------------------------------------------
+
+# Create new file with 'My Object JSON'(my_object_json)
+fd, path_tmp_file = tempfile.mkstemp()
+try:
+    with os.fdopen(fd, 'w') as tmp:
+        # create temp file with object Json (Kubernetes object)
+        tmp.write(my_object_json)
+        tmp.flush()
+        print(f"\n\npath_tmp_file::: {path_tmp_file}")
+finally:
+    pass # For tests
+    # os.remove(path_tmp_file)
+    
+# ------------------------------------------------------------
+
+print(f"\n\njson.dumps::: {my_object_json}")
+print(f"\n\nyaml.dump::: {yaml.dump(my_object_json)}")
+
+# ------------------------------------------------------------
+
+# Load kube_config
+config.load_kube_config()
+# Get ApiClient (with defult configuration)
+k8s_client = client.ApiClient()
+
+# Apply file in Kubernetes 
+utils.create_from_yaml(k8s_client, path_tmp_file)
+
+# Delete tmp_file after apply on Kubernetes
+os.remove(path_tmp_file)
